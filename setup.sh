a5768ad7c01369df57f43e03cb4294f14c18365c
jayqclone <jayrothman@gmail.com>
2025-05-28 16:35:25 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+diff --git a//dev/null b/setup.sh
+index 0000000000000000000000000000000000000000..7b7be9ebf65ed2d49bc0636bbbc7c04b30347714 100755
+--- a//dev/null
++++ b/setup.sh
+@@ -0,0 +1,4 @@
++#!/bin/bash
++
++# Install Node dependencies with network access
++npm ci
